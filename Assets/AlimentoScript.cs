24612d4fae3f1dff6f24edebf996bb886948b7af
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-06-06 10:49:06 -0300

    1er clase completa

--- Assets/AlimentoScript.cs
+++ Assets/AlimentoScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AlimentoScript : MonoBehaviour
 {
+    public int valorAlimentario;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public int nivelHambre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+        if (nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        if (alimento)
+        {
+            TomarAlimento(alimento.valorAlimentario);
+            Destroy(other.gameObject);
+        }
+    }
+}
